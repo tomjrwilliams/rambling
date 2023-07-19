1c0071d9f1ec9982271966ff9ebfce2d1d8da415
Tom Williams <tom.williams@havencove.com>
2023-07-19 16:44:11 +0100

    some renaming

---
+++ src/equity.py
@@ -0,0 +1,22 @@
+def in_universe(ticker, df, threshold):
+    if ticker not in df.columns:
+        return False
+    return (
+        df[ticker].sum() / len(df.index)
+    ) >= threshold
+
+# pca, _ = fit_pca(d_start, d_end, n)
+# dfs = dfs_indices
+# dfs = dfs_sectors
+# universe = bt.algos.universe.configs.INDICES
+# universe = bt.algos.universe.configs.GICS_SECTORS
+def f_ticker_map(dfs, f_model, f_tickers, threshold = 1.):
+    def f(*args, **kwargs):
+        model = f_model(*args, **kwargs)
+        tickers = f_tickers(model)
+        return {
+            i: tickers.filter(
+                in_universe, df = dfs[i], threshold=threshold
+            ) for i in sorted(dfs.keys())
+        }
+    return f
